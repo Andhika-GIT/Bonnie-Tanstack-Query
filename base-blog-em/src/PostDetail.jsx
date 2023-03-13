24823bb2adc37fff65a555a8aba6b4d14db35467
Andhika Prasetya <andhikapr081@gmail.com>
2023-03-13 20:22:51 +0700

    base-blog-em query keys dependency

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,33 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
   return response.json();
 }
 
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, { method: 'DELETE' });
   return response.json();
 }
 
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, { method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } });
   return response.json();
 }
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, error, isLoading, isError } = useQuery({ queryKey: ['post-comment', post.id], queryFn: () => fetchComments(post.id) });
+
+  if (isError) return <h3>oops there's something wrong, {error}</h3>;
+  if (isLoading) return <h3>loading...</h3>;
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -6,17 +6,17 @@ import { useQuery } from '@tanstack/react-query';
 
 const maxPostPage = 10;
 
-async function fetchPosts() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0');
+async function fetchPosts(pageNum) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`);
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data, isError, error, isLoading } = useQuery({ queryKey: ['posts'], queryFn: fetchPosts, staleTime: 5000 });
+  const { data, isError, error, isLoading } = useQuery({ queryKey: ['posts', currentPage], queryFn: () => fetchPosts(currentPage), staleTime: 5000 });
 
   if (isError) return <h3>Oops, something when wrong {error.toString()}</h3>;
   if (isLoading) return <h3>loading...</h3>;
@@ -31,11 +31,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prevPage) => prevPage - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= 10}
+          onClick={() => {
+            setCurrentPage((prevPage) => prevPage + 1);
+          }}
+        >
           Next page
         </button>
       </div>
