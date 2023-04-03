fe01a926e1f13bc9e09d0eabe0173e4cf273f0a4
Andhika Prasetya <andhikapr081@gmail.com>
2023-04-03 21:32:57 +0700

    base-lazy-days global re-fetch options

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -19,6 +19,15 @@ function queryErrorHandler(error: unknown): void {
 
 // to satisfy typescript until this file has uncommented contents
 export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 600000, // 10 minutes
+      cacheTime: 900000, // 15 minutes
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    },
+  },
   queryCache: new QueryCache({
     onError: queryErrorHandler,
   }),
