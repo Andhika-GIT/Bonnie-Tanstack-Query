860ff3c97e5ac6740f3a52282805fb23dcbeea3f
Andhika Prasetya <andhikapr081@gmail.com>
2023-04-08 08:46:49 +0700

    base-lazy-days writing optimistic update update the query if sucessfull revert back to previous cache if failed

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -5,8 +5,13 @@ import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
 import { useUser } from './useUser';
 
-import { useMutation, UseMutateFunction } from '@tanstack/react-query';
+import {
+  useMutation,
+  UseMutateFunction,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { useToast } from '@chakra-ui/react';
+import { queryKeys } from 'react-query/constants';
 
 // for when we need a server function
 async function patchUserOnServer(
@@ -29,26 +34,59 @@ async function patchUserOnServer(
 }
 
 // TODO: update type to UseMutateFunction type
-export function usePatchUser(): (newData: User | null) => void {
+export function usePatchUser(): UseMutateFunction<
+  User,
+  unknown,
+  User,
+  unknown
+> {
   const { user, updateUser } = useUser();
   const toast = useToast();
+  const queryClient = useQueryClient();
 
   // TODO: replace with mutate function
-  const { mutate: patchUser } = useMutation(
-    (newData: User) => patchUserOnServer(newData, user),
-    {
-      // userData -> the result from patchUserOnServer(newData,user) when calling useMutation
-      onSuccess: (userData: User | null) => {
-        if (userData) {
-          updateUser(userData);
-          toast({
-            title: 'User sucessfully updated!',
-            status: 'success',
-          });
-        }
-      },
+  const { mutate: patchUser } = useMutation({
+    mutationFn: (newData: User) => patchUserOnServer(newData, user),
+
+    onMutate: async (newData: User | null) => {
+      // Cancel any outgoing refetches
+      await queryClient.cancelQueries({ queryKey: [queryKeys.user] });
+
+      //  save snapshot of previous user value
+      const previousUserData: User = queryClient.getQueryData([queryKeys.user]);
+
+      // optimistically update the cache with new user value
+      queryClient.setQueryData([queryKeys.user], newData);
+
+      // Return a context with the previous and new todo
+      return { previousUserData };
     },
-  );
+    onError: (error, newData, context) => {
+      // roll back cache to saved value
+
+      if (context.previousUserData) {
+        queryClient.setQueryData([queryKeys.user], context.previousUserData);
+        toast({
+          title: `update failed : ${error}, restoring previous values`,
+          status: 'warning',
+        });
+      }
+    },
+    onSuccess: (userData: User | null) => {
+      // userData -> the result from patchUserOnServer(newData,user) when calling useMutation
+      if (userData) {
+        updateUser(userData);
+        toast({
+          title: 'User sucessfully updated!',
+          status: 'success',
+        });
+      }
+    },
+    onSettled: () => {
+      // invalidate user query to get the latest data from server
+      queryClient.invalidateQueries({ queryKey: [queryKeys.user] });
+    },
+  });
 
   return patchUser;
 }
